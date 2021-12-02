6ad8232852d1f404291560db54219a9f103474ab
Jack Riley <mbp20jjr@sharc-node002.shef.ac.uk>
2021-12-02 15:40:18 +0000

    Automatically determine most stable isoform and normalize to it

--- calc_ddCT.R
+++ calc_ddCT.R
@@ -23,12 +23,29 @@ if(str_detect(file_input[1], "iu")==TRUE & str_detect(file_input[2], "ss")==TRUE
 spliced = spliced %>% separate(Name, c("gene", "isoform", "time", "rep"), " ")
 retained = retained %>% separate(Name, c("gene", "isoform", "time", "rep"), " ")
 
-#Get average of spliced for each timepoint so can do deltaCT
+#Get average of spliced and retained for each timepoint so can work out which is the most stable and then do deltaCT
 mean_spliced = spliced %>% group_by(time) %>% summarize(meanCT_spliced = mean(Ct))
+mean_retained = retained %>% group_by(time) %>% summarize(meanCT_retained = mean(Ct))
 
-#Do delta CT against spliced
-spliced = left_join(spliced, mean_spliced, by="time") %>% mutate(dCT = Ct-meanCT_spliced)
-retained = left_join(retained, mean_spliced, by="time") %>% mutate(dCT = Ct-meanCT_spliced)
+spliced_first_time = mean_spliced[1,2] %>% unlist() %>% unname()
+spliced_last_time = mean_spliced[nrow(mean_spliced),2] %>% unlist() %>% unname()
+abs_dif_spliced = abs(spliced_first_time-spliced_last_time)
+
+retained_first_time = mean_retained[1,2] %>% unlist() %>% unname()
+retained_last_time = mean_retained[nrow(mean_retained),2] %>% unlist() %>% unname()
+abs_dif_retained = abs(retained_first_time-retained_last_time)
+
+if(abs_dif_spliced<abs_dif_retained){
+  most_stable = mean_spliced
+}else if(abs_dif_retained<abs_dif_spliced){
+  most_stable = mean_retained
+}
+
+colnames(most_stable)[2]="most_stable_CT"
+
+#Do delta CT against most stable
+spliced = left_join(spliced, most_stable, by="time") %>% mutate(dCT = Ct-most_stable_CT)
+retained = left_join(retained, most_stable, by="time") %>% mutate(dCT = Ct-most_stable_CT)
 
 #Now need to do ddCT for each isoform against timepoint 0 of its condition
 dCT0_spliced = spliced %>% 
@@ -49,8 +66,8 @@ spliced = spliced %>% add_column(dCT0_spliced=dCT0_spliced)
 retained = retained %>% add_column(dCT0_retained=dCT0_retained)
 
 #ddCT calculation
-spliced = spliced %>% mutate(ddCT = dCT - dCT0_spliced) %>% select(-c(meanCT_spliced, dCT0_spliced))
-retained = retained %>% mutate(ddCT = dCT - dCT0_retained) %>% select(-c(meanCT_spliced, dCT0_retained))
+spliced = spliced %>% mutate(ddCT = dCT - dCT0_spliced) %>% select(-c(most_stable_CT, dCT0_spliced))
+retained = retained %>% mutate(ddCT = dCT - dCT0_retained) %>% select(-c(most_stable_CT, dCT0_retained))
 
 #2^-ddCT calculation
 spliced = spliced %>% mutate(`2^-ddCT`=2^-ddCT)
